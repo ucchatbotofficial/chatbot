0032800fca4cadcb387497175b48e333cee8695d
Savitha <savitha.saviy@gmail.com>
2025-08-30 12:00:54 +0530

    old

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -242,7 +242,7 @@ function App() {
               </div>
             </div>
             <div className="chatbot-header-right">
-              {/* <button className="chatbot-icon-btn-modern" title="Minimize">
+              <button className="chatbot-icon-btn-modern" title="Minimize">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M6 12H18" stroke="#666" strokeWidth="2" strokeLinecap="round"/>
                 </svg>
@@ -258,7 +258,7 @@ function App() {
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M18 6L6 18M6 6L18 18" stroke="#666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
-              </button> */}
+              </button>
             </div>
           </div>
           
